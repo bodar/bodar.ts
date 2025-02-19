6c48e16a3404230f05bc459f5dbf675be5ee5765
Daniel Bodart <dan@bodar.com>
2025-02-19 07:39:05 +0000

    Switch to using bun for build

--- commands.sh
+++ commands.sh
@@ -56,3 +56,9 @@ function bun() {
   bun "$@"
 }
 export -f bun
+
+# If this script is being executed (not sourced) and has an argument, run it with bun
+if [[ "${BASH_SOURCE[0]}" == "${0}" && -n "${1}" ]]; then
+    bun "$@"
+    exit $?
+fi
