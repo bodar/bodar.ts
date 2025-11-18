4e851f19aaf4186a6720aa38ec2775bcbc06396f
Daniel Worthington-Bodart <dan@bodar.com>
2025-11-18 07:15:36 +0000

    Extracted event iterator

--- packages/dataflow/src/Mutation.ts
+++ packages/dataflow/src/Mutation.ts
@@ -12,18 +12,22 @@ export class Mutation<T> extends EventTarget implements AsyncIterable<T> {
         this.dispatchEvent(new CustomEvent('change', {detail: {value: value}}));
     }
 
-    async* [Symbol.asyncIterator](): AsyncIterator<T> {
-        let {promise, resolve} = Promise.withResolvers();
-        // Must close over the resolve variable to see it change
-        this.addEventListener('change', () => resolve())
+    [Symbol.asyncIterator]() {
+        return eventListener(this, 'change', (ev: any) => ev.detail.value, this._value);
+    }
+}
 
-        yield this._value;
+export async function* eventListener<E extends Event, R>(source: EventTarget, eventType: string, eventHandler: (ev: E) => R, value: R): AsyncIterator<R> {
+    let {promise, resolve} = Promise.withResolvers<R>();
+    // Must close over the resolve variable to see it change
+    source.addEventListener(eventType, (ev: any) => resolve(value = eventHandler(ev)))
 
-        while (true) {
-            await promise;
-            // Must create new promise before yielding otherwise we miss the synchronous event dispatch
-            ({promise, resolve} = Promise.withResolvers());
-            yield this._value;
-        }
+    yield value;
+
+    while (true) {
+        await promise;
+        // Must create new promise before yielding otherwise we miss the synchronous event dispatch
+        ({promise, resolve} = Promise.withResolvers<R>());
+        yield value;
     }
 }
