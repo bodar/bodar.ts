3b2cf549cd390eb245784e13472cf7a0f289259c
Daniel Worthington-Bodart <dan@bodar.com>
2025-11-19 13:22:41 +0000

    dataflow: added overload for Graph.define

--- packages/dataflow/src/Graph.ts
+++ packages/dataflow/src/Graph.ts
@@ -6,6 +6,7 @@ import {simpleHash} from "./simpleHash.ts";
 import {getInputs, getOutputs, parseFunction} from "./function-parsing.ts";
 import {node, Node} from "./Node.ts";
 import {Backpressure, type BackpressureStrategy} from "./SharedAsyncIterable.ts";
+import {lazy} from "@bodar/totallylazy/functions/lazy.ts";
 
 /** Manages a graph of reactive nodes with automatic dependency tracking */
 export class Graph {
@@ -15,13 +16,12 @@ export class Graph {
     /** Creates nodes from a function, parsing inputs/outputs to build the dependency graph */
     define(fun:Function): { [id: string]: Node<any> };
     define(key: string, fun: Function): { [id: string]: Node<any> };
+    define(key: string, inputs: string[], outputs: string[], fun: Function): { [id: string]: Node<any> };
     define(...args:any[]): { [id: string]: Node<any> } {
         const fun = args.find(v => typeof v === "function")!;
         const key = args.find(v => typeof v === 'string') || (fun.name === '' ? simpleHash(fun.toString()) : fun.name);
-        const definition = parseFunction(fun);
-        const inputs = getInputs(definition);
-        const outputs = getOutputs(definition);
-
+        const definition = lazy(() =>  parseFunction(fun));
+        const [inputs = getInputs(definition), outputs = getOutputs(definition)] = args.filter(Array.isArray) as string[][];
         return Object.fromEntries([
             [key, this.set(key, inputs, fun)],
             ...outputs.map(output => [output, this.set(output, [key], (result: any) => Reflect.get(result, output))])
