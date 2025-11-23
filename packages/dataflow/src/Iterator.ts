5a16c61a9aad148695abe4bbf13538db2a66a610
Daniel Worthington-Bodart <dan@bodar.com>
2025-11-23 09:47:11 +0000

    First pass at a PushNode

--- packages/dataflow/src/Iterator.ts
+++ packages/dataflow/src/Iterator.ts
@@ -3,12 +3,12 @@
  * */
 
 /** Converts a callback into an AsyncIterator */
-export async function* iterator<T>(init: (notify: (e: T) => any) => any, value: T): AsyncIterator<T> {
+export async function* iterator<T>(init: (notify: (e: T) => any) => any, value?: T): AsyncIterator<T> {
     let {promise, resolve} = Promise.withResolvers<T>();
     // Must close over the resolve variable to see it change
     init((v => resolve(value = v)))
 
-    yield value;
+    if(value !== undefined) yield value;
 
     while (true) {
         await promise;
---
+++ packages/dataflow/src/PushNode.ts
@@ -0,0 +1,91 @@
+/**
+ * Reactive nodes that combine dependency streams and yield computed values
+ * @module
+ */
+import {isAsyncGeneratorFunction, isAsyncIterable, isAsyncIterator, isGeneratorFunction} from "./type-guards.ts";
+import type {ThrottleStrategy} from "./Throttle.ts";
+import {iterator} from "./Iterator.ts";
+
+export interface EventData<T> {
+    key: string;
+    value: T;
+}
+
+/** A push / event based Node*/
+export class PushNode<T> extends EventTarget implements AsyncIterable<EventData<T>> {
+    private lastInputs = new Map<string, any>();
+
+    constructor(public key: string, public dependencies: PushNode<any>[], public fun: Function,
+                private throttle: ThrottleStrategy) {
+        super();
+        this.lastInputs = new Map(this.dependencies.map(d => {
+            d.addEventListener('change', (ev: any) => this.execute(ev.detail))
+            return [d.key, undefined];
+        }))
+    }
+
+    [Symbol.asyncIterator](): AsyncIterator<EventData<T>> {
+        return iterator(notify => this.addEventListener('change', (ev: any) => notify(ev.detail)));
+    }
+
+    private lastResult?: any;
+
+    execute(data: EventData<T>) {
+        const oldValue = this.lastInputs.get(data.key);
+        if (oldValue != data.value) {
+            this.lastInputs.set(data.key, data.value);
+            const currentValues = Array.from(this.lastInputs.values());
+            if(currentValues.every(v => v !== undefined)) {
+                this.lastResult = this.fun(...currentValues);
+                this.processResult(this.lastResult)
+            }
+        }
+    }
+
+    async processResult(result: any): Promise<void> {
+        const iterable = this.getIterable(result);
+        if (iterable) {
+            for await (const value of iterable) {
+                // Throttle first so any synchronous events can arrive
+                await this.throttle();
+                this.dispatchEvent(new CustomEvent<EventData<T>>('change', {detail: {key: this.key, value}}));
+            }
+        } else {
+            this.dispatchEvent(new CustomEvent<EventData<T>>('change', {detail: {key: this.key, value: result}}));
+        }
+    }
+
+    getIterable(result: any): AsyncIterable<T> | undefined {
+        if (isAsyncIterable(result)) {
+            return result;
+        } else if (isAsyncIterator(result)) {
+            return {
+                [Symbol.asyncIterator]() {
+                    return result;
+                }
+            };
+        } else if (isAsyncGeneratorFunction(result) && result.length === 0) {
+            return {
+                [Symbol.asyncIterator]() {
+                    return result() as AsyncGenerator<T>;
+                }
+            };
+        } else if (isGeneratorFunction(result) && result.length === 0) {
+            const iterator = result() as Generator<T>;
+            return {
+                async* [Symbol.asyncIterator]() {
+                    yield* {
+                        [Symbol.iterator]() {
+                            return iterator
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
+
+/** Factory function to create a new reactive node */
+export function node<T>(key: string, dependencies: PushNode<any>[], fun: Function, throttle: ThrottleStrategy): PushNode<T> {
+    return new PushNode(key, dependencies, fun, throttle)
+}
