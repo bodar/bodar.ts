c0e031a3b5a26fb0faaa320892fc47968e80ec80
Daniel Worthington-Bodart <dan@bodar.com>
2025-12-03 22:55:24 +0000

    dataflow: add tests for Renderer

--- packages/dataflow/src/html/Renderer.ts
+++ packages/dataflow/src/html/Renderer.ts
@@ -3,48 +3,64 @@
  * **/
 import {BaseGraph} from "../BaseGraph.ts";
 
+export interface RendererDependencies {
+    graph: BaseGraph,
+    document: Document,
+    Node: typeof Node,
+    DocumentFragment: typeof DocumentFragment,
+}
+
+const DefaultDependencies: RendererDependencies = {
+    graph: new BaseGraph(),
+    document: globalThis.document,
+    Node: globalThis.Node,
+    DocumentFragment: globalThis.DocumentFragment
+};
+
 /** Renderer **/
 export class Renderer {
-    constructor(private graph: BaseGraph = new BaseGraph(), private doc: Document = document) {
+    constructor(private deps: RendererDependencies = DefaultDependencies) {
     }
 
-    async register(key: string, inputs: string[], outputs: string[], fun: Function) {
-        this.graph.define(key, inputs, outputs, fun);
+    register(key: string, inputs: string[], outputs: string[], fun: Function): void {
+        this.deps.graph.define(key, inputs, outputs, fun);
     }
 
-    render() {
-        this.graph.sinks().forEach(async (node) => {
-            const slot = this.doc.querySelector(`slot[name="${node.key}"]`);
+    render(): void {
+        const {document, DocumentFragment} = this.deps;
+        this.deps.graph.sinks().forEach(async (node) => {
+            const slot = document.querySelector(`slot[name="${node.key}"]`);
             for await (const {value: update} of node) {
                 if (slot) {
                     const newNode = this.createUpdateNode(update);
                     if (!newNode) continue;
-                    // if (newNode instanceof DocumentFragment) {
-                    //     if (slot.childNodes.length === newNode.childNodes.length) {
-                    //         for (let i = 0; i < slot.childNodes.length; i++) {
-                    //             const slotChild = slot.childNodes[i];
-                    //             const newChild = newNode.childNodes[i];
-                    //             if (!slotChild.isEqualNode(newChild)) slotChild.replaceWith(newNode);
-                    //         }
-                    //         continue;
-                    //     }
-                    // }
-                    // if (slot.childNodes.length !== 1 || !slot.childNodes[0].isEqualNode(newNode)) slot.replaceChildren(newNode);
-                    slot.replaceChildren(newNode);
+                    if (newNode instanceof DocumentFragment && slot.childNodes.length === newNode.childNodes.length) {
+                        for (let i = 0; i < slot.childNodes.length; i++) {
+                            const slotChild = slot.childNodes[i];
+                            const newChild = newNode.childNodes[i];
+                            if (!slotChild.isEqualNode(newChild)) slotChild.replaceWith(newChild);
+                        }
+                    } else if (slot.childNodes.length === 1 && !(newNode instanceof DocumentFragment)) {
+                        if (!slot.childNodes[0].isEqualNode(newNode)) slot.replaceChildren(newNode);
+                    } else {
+                        slot.replaceChildren(newNode);
+                    }
+
                 }
             }
         });
     }
 
     private createUpdateNode(update: any): Node | undefined {
+        const {document, Node} = this.deps;
         if (Array.isArray(update)) {
-            const fragment = this.doc.createDocumentFragment();
+            const fragment = document.createDocumentFragment();
             fragment.replaceChildren(...update.map(u => this.createUpdateNode(u)).filter(u => u !== undefined));
             return fragment;
         } else if (typeof update === "string") {
-            return this.doc.createTextNode(update);
+            return document.createTextNode(update);
         } else if (typeof update === "number") {
-            return this.doc.createTextNode(String(update));
+            return document.createTextNode(String(update));
         }
         if (update instanceof Node) {
             return update;
--- packages/dataflow/test/html/Renderer.test.ts
+++ packages/dataflow/test/html/Renderer.test.ts
@@ -1,7 +1,56 @@
 import {describe, it} from "bun:test";
+import {parseHTML} from "linkedom";
+import {Renderer} from "../../src/html/Renderer.ts";
+import {BaseGraph} from "../../src/BaseGraph.ts";
+import {is} from "@bodar/totallylazy/predicates/IsPredicate.ts";
+import {assertFalse, assertThat} from "@bodar/totallylazy/asserts/assertThat.ts";
 
 describe("Renderer", () => {
-    it("", () => {
+    async function render(fun: (doc: Document) => any, initalStart: string = ''): Promise<Element> {
+        const globals = parseHTML(`<slot name="output">${initalStart}</slot>`);
+        Reflect.set(globals, 'graph', new BaseGraph());
+        const renderer = new Renderer(globals as any);
 
+        renderer.register('output', [], [], () => fun(globals.document));
+        renderer.render();
+
+        await new Promise(resolve => setTimeout(resolve, 0));
+        return globals.document.querySelector('slot[name="output"]')!;
+    }
+
+    it("Can render a string", async () => {
+        assertThat((await render(() => 'Hello, world!')).innerHTML, is('Hello, world!'));
+    });
+
+    it("Can render a number", async () => {
+        assertThat((await render(() => 123)).innerHTML, is('123'));
+    });
+
+    it("Can render a element", async () => {
+        assertThat((await render(document => document.createElement('div'))).innerHTML, is('<div></div>'));
+    });
+
+    it("Can render an array of nodes", async () => {
+        assertThat((await render(document => [
+            document.createElement('div'),
+            document.createTextNode('Hello, world!'),
+            document.createElement('span')
+        ])).innerHTML, is('<div></div>Hello, world!<span></span>'));
+    });
+
+    it("Only updates nodes if they are different", async () => {
+        let elememt: HTMLElement | undefined;
+        const children = (await render(document => {
+            elememt = document.createElement('div');
+            elememt.setAttribute('id', 'test');
+            // Node.isEqualNode won't see this custom property
+            // so it will think the nodes are the same and not replace it
+            Reflect.set(elememt, '@tag@', 'new');
+            return elememt;
+            },
+            '<div id="test"></div>')).childNodes;
+        // If it had replaced it, then this will be true
+        const newChild = children[0];
+        assertFalse(Reflect.get(newChild, '@tag@') === 'new');
     });
 });
