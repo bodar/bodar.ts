1e963a4c07787764b4009375e29662c168b9b195
Daniel Worthington-Bodart <dan@bodar.com>
2026-01-06 12:50:39 +0000

    dataflow: cleanup equality checking as jsx2dom already correctly handles style difference

--- packages/dataflow/src/html/SlotRenderer.ts
+++ packages/dataflow/src/html/SlotRenderer.ts
@@ -32,24 +32,11 @@ export class SlotRenderer {
 
     updateSlot(slot: HTMLSlotElement, newNodes: Node[]) {
         while (slot.childNodes.length > newNodes.length) slot.removeChild(slot.lastChild!);
-        const slotChildren = Array.from(slot.childNodes);
         for (let i = 0; i < newNodes.length; i++) {
             const newChild = newNodes[i];
-            const slotChild = slotChildren[i];
+            const slotChild = slot.childNodes[i];
             if (slotChild === undefined) slot?.appendChild(newChild);
-            else if (!this.nodeEquality(newChild, slotChild)) slotChild.replaceWith(newChild);
+            else if (!newChild.isEqualNode(slotChild)) slotChild.replaceWith(newChild);
         }
-    }
-
-    nodeEquality(a: Node, b: Node) {
-        const {HTMLElement} = this.deps;
-        return a instanceof HTMLElement && b instanceof HTMLElement
-            ? this.elementEquality(a, b)
-            : a.isEqualNode(b);
-    }
-
-    elementEquality(a: HTMLElement, b: HTMLElement) {
-        if (a.hasAttribute('data-unique') || b.hasAttribute('data-unique')) return false;
-        return a.isEqualNode(b) && a.style.cssText === b.style.cssText;
     }
 }
